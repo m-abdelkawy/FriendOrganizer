5b267ad294a8960b69f045ce17d712dbcc57dded
m-abdelkawy <engineer_m.abdelkawy@yahoo.com>
2021-01-03 13:38:29 +0200

    initial architecture

---
+++ FriendOrganizer/FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,16 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend() { FirstName = "hamada 1", LastName = "el gen 1" };
+            yield return new Friend() { FirstName = "hamada 2", LastName = "el gen 2" };
+            yield return new Friend() { FirstName = "hamada 3", LastName = "el gen 3" };
+            yield return new Friend() { FirstName = "hamada 4", LastName = "el gen 4" };
+        }
+    }
+}
---
+++ FriendOrganizer/FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data;
+using System.Collections.ObjectModel;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public class MainViewModel
+    {
+        public IFriendDataService _friendDataService;
+        private Friend _selectedFriend;
+
+        public Friend SelectedFriend
+        {
+            get { return _selectedFriend; }
+            set { _selectedFriend = value; }
+        }
+            
+        public ObservableCollection<Friend> Friends { get; set; }
+
+
+        public MainViewModel(IFriendDataService friendDataService)
+        {
+            Friends = new ObservableCollection<Friend>();
+            _friendDataService = friendDataService;
+        }
+
+        public void Load()
+        {
+            var lstFriend = _friendDataService.GetAll();
+            Friends.Clear();
+            foreach (var friend in lstFriend)
+            {
+                Friends.Add(friend);
+            }
+        }
+        
+    }
+}
